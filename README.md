624be83e57510467237bfe5dc674fa2e1c06a0d0
Rita Ferreira <rita.c.ferreira@live.com>
2022-07-20 20:25:15 +0100

    clock face pale
    v.1.0.3

---
+++ README.md
@@ -0,0 +1,27 @@
+# Simple clock face - Simple pale
+
+Simple experimental clock face for fitbit OS.
+## Includes:
+
+❤️ heart rate, 
+
+👣 daily step count,
+
+🕑 current date and time.
+
+Supports both 12h/24h format.
+Only english supported locale.
+Works in Sense and Versa 3.
+
+---
+
+## Link to gallery: 
+### [Clock face](https://gallery.fitbit.com/details/d12bc537-f262-4f53-a99c-b95bd8dbf4a6)
+
+<br>
+
+# Screenshot
+![Screenshot Sense!](/screenshots/Screenshot.png "Screenshot Sense")
+
+# Credits
+* Inspired by [notwaldorf](https://mobile.twitter.com/notwaldorf/status/1222766539981459456?lang=da) clock face.
---
+++ app/index.js
@@ -0,0 +1,60 @@
+import clock from "clock";
+import { display } from "display";
+import * as document from "document";
+import { preferences } from "user-settings";
+import * as util from "../common/utils";
+import { HeartRateSensor } from "heart-rate";
+import { me as appbit } from "appbit";
+import { today } from "user-activity";
+
+clock.granularity = "minutes";
+
+const myLabel = document.getElementById("myLabel");
+const myDate = document.getElementById("myDate");
+const myHeart = document.getElementById("myHeart");
+const mySteps = document.getElementById("mySteps");
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const monthsShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const updateSteps = () => {
+  if (appbit.permissions.granted("access_activity")) {
+    let steps = today.adjusted.steps;
+    let newSteps = steps > 9999 ? (Math.floor(steps/1000) + 'k') : steps
+
+    mySteps.text = newSteps
+  }
+}
+
+clock.ontick = (evt) => {
+  let today = evt.date;
+  let hours = today.getHours();
+  let dayName = days[today.getDay()];
+  let monthNameShort = monthsShort[today.getMonth()];
+  let dayNumber = util.zeroPad(today.getDate());
+
+  if (preferences.clockDisplay === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
+  } else {
+    // 24h format
+    hours = util.zeroPad(hours);
+  }
+  let mins = util.zeroPad(today.getMinutes());
+  myDate.text = `${dayName}, ${monthNameShort} ${dayNumber}`;
+  myLabel.text = `${hours}:${mins}`;
+  updateSteps();
+}
+
+if (HeartRateSensor) {
+   const hrm = new HeartRateSensor();
+   hrm.addEventListener("reading", () => {
+     myHeart.text = `${hrm.heartRate}`;
+   });
+   hrm.start();
+} else {
+   console.log("This device does NOT have a HeartRateSensor!");
+}
+
+
+
